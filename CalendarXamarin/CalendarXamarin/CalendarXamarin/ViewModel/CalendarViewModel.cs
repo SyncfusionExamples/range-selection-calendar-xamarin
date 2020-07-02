b27971a8bacb90a6f4e7092b38909b2e55ee057a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-02 17:40:22 +0530

    XAMARIN-34939 - range selection sample added

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class CalendarViewModel
+    {
+        public SelectionRange SelectedRange { get; set; }
+        public CalendarViewModel()
+        {
+            SelectedRange = new SelectionRange();
+            SelectedRange.StartDate = new DateTime(2019, 09, 16);
+            SelectedRange.EndDate = new DateTime(2019, 09, 20);
+
+        }
+    }
+}
+
